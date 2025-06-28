a77b31b5520eda736f3bcfa1521b46bd9f23a829
Josh Lee <leejosh12@gmail.com>
2025-06-27 20:52:25 -0500

    I don't know what went wrong. I hope this works

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StbImageSharp;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,17 +16,32 @@ namespace ProtoCraft
     public class Game : GameWindow
     {
 
+       
         float[] vertices = {
-          0.0f,  0.5f, 0.0f,   // top
-          0.5f,  0.0f, 0.0f,   // right
-          0.0f, -0.5f, 0.0f,   // bottom
-          -0.5f,  0.0f, 0.0f    // left
+         -0.5f,  0.5f, 0f,  // top-left
+          0.5f,  0.5f, 0f,  // top-right
+          0.5f, -0.5f, 0f,  // bottom-right
+         -0.5f, -0.5f, 0f   // bottom-left
          };
 
-        // VAO, VBO, EBO, and Shader Program variables
+        float[] texCoords = {
+            0f, 1f, // top-left
+            1f, 1f, // top-right
+            1f, 0f, // bottom-right
+            0f, 0f  // bottom-left
+        };
+
+        uint[] indices = {
+            0, 1, 2, // top triangle
+            2, 3, 0  // bottom triangle
+        };
         //Render Pipeline vars
         int vao;
         int shaderProgram;
+        int vbo;
+        int ebo; //element buffer object for indices
+        int textureID; //texture ID for the texture we will load
+        int textureVBO; //texture VBO for texture coordinates
 
         // everything involving height and width should be in here 
         int width, height;
@@ -51,17 +67,42 @@ namespace ProtoCraft
                 base.OnLoad();
 
                 vao = GL.GenVertexArray();
+
+                 // need to bind vao
+                GL.BindVertexArray(vao);
                 
-                int vbo = GL.GenBuffer();
+                vbo = GL.GenBuffer();
                 GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
                 GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-                // need to bind vao
-                GL.BindVertexArray(vao);
+                
+               // put the vertex VBo
+
+
+                // Slot 0  
                 GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
                 GL.EnableVertexArrayAttrib(vao, 0);
+
+               GL.BindBuffer(BufferTarget.ArrayBuffer, 0); // unbind the VBO
+
+                //Slot 1 for texture coordinates
+                GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 0, 0);
+                GL.EnableVertexArrayAttrib(vao, 1);
+
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0); // unbind the VBO
+
+                //Texture Coordinates
+                textureVBO = GL.GenBuffer();
+                GL.BindBuffer(BufferTarget.ArrayBuffer, textureVBO);
+                GL.BufferData(BufferTarget.ArrayBuffer, texCoords.Length * sizeof(float), texCoords, BufferUsageHint.StaticDraw);
                 //Load shader program
                 GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
                 GL.BindVertexArray(0);
+
+                ebo = GL.GenBuffer();
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+                GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+
 
                 //Shader Program
                 shaderProgram = GL.CreateProgram();
@@ -81,12 +122,38 @@ namespace ProtoCraft
                 //delete shaders after linking
                 GL.DeleteShader(vertexShader);
                 GL.DeleteShader(fragmentShader);
+
+                // ---- TEXTURE LOADING ----
+                textureID = GL.GenTexture();
+                // activate the texture unit
+                GL.ActiveTexture(TextureUnit.Texture0);
+                GL.BindTexture(TextureTarget.Texture2D, textureID);
+
+                // texture parameters
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+                // load the texture image
+                StbImage.stbi_set_flip_vertically_on_load(1); // flip the image vertically
+                ImageResult dirtTexture = ImageResult.FromStream(File.OpenRead("../../../Textures/tnt_top.png"), ColorComponents.RedGreenBlueAlpha);
+
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, dirtTexture.Width, dirtTexture.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, dirtTexture.Data);
+                // unbind the texture
+                GL.BindTexture(TextureTarget.Texture2D, 0);
+
         }     
 
 
-            protected override void OnUnload()
+        protected override void OnUnload()
             {
                 base.OnUnload();
+                //Delete the buffers and shader program
+                GL.DeleteVertexArray(vao);
+                GL.DeleteBuffer(vbo);
+                GL.DeleteBuffer(ebo);
+                GL.DeleteTexture(textureID);
+                GL.DeleteProgram(shaderProgram);
             }
         protected override void OnRenderFrame(FrameEventArgs args)
             {
@@ -96,8 +163,13 @@ namespace ProtoCraft
 
                 //The triangle
                 GL.UseProgram(shaderProgram);
+                
+                GL.BindTexture(TextureTarget.Texture2D, textureID);
+
                 GL.BindVertexArray(vao);
-                GL.DrawArrays(PrimitiveType.TriangleFan, 0, 4);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+                GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+                //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
                     
                 Context.SwapBuffers();
                 base.OnRenderFrame(args);
--- Shaders/Default.frag
+++ Shaders/Default.frag
@@ -1,7 +1,11 @@
 ﻿#version 330 core
+
+in vec2 TexCoords; 
 out vec4 FragColor;
+
+uniform sampler2D texture0;
 
 void main()
 {
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0); // red color
+    FragColor = texture(texture0, texCoord);
 }
